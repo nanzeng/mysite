8c68bee8e369aac1926ea9b95dc1b19a5b877850
Nan Zeng <zengnan@gmail.com>
2016-12-22 23:09:29 +1100

    add home page function

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from lists import views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^polls/', include('polls.urls', namespace='polls')),
+    url(r'^$', views.home_page, name='home'),
 ]
